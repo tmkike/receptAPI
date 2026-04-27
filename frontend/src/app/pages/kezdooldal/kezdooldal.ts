7e1b4a80bcdd751a858dd832af4b8adb6e4141f2
tmkike <tmkike08@gmail.com>
2026-04-27 19:26:55 +0200

    Kezdőoldal feltöltése

--- frontend/src/app/pages/kezdooldal/kezdooldal.ts
+++ frontend/src/app/pages/kezdooldal/kezdooldal.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-kezdooldal',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './kezdooldal.html',
   styleUrl: './kezdooldal.scss',
 })
