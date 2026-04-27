891675392335bce1eeef877f80831437fdda844c
tmkike <tmkike08@gmail.com>
2026-04-27 20:34:39 +0200

    login felület

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -10,5 +10,5 @@ import { NavbarComponent } from './components/navbar/navbar';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Recept Tár');
 }
--- frontend/src/app/pages/bejelentkezes/bejelentkezes.ts
+++ frontend/src/app/pages/bejelentkezes/bejelentkezes.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-bejelentkezes',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './bejelentkezes.html',
   styleUrl: './bejelentkezes.scss',
 })
