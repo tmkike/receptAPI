aad4b8aa9007caf05b2bb23ae213ac0bfe651c4a
tmkike <tmkike08@gmail.com>
2026-04-27 20:40:40 +0200

    reg oldal

--- frontend/src/app/pages/regisztracio/regisztracio.ts
+++ frontend/src/app/pages/regisztracio/regisztracio.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-regisztracio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './regisztracio.html',
   styleUrl: './regisztracio.scss',
 })
